5044c64882abba10743d15dfcf0f831156137807
benradspinner <benradspinner@gmail.com>
2025-10-31 16:06:33 -0400

    Update subscaleFlight.py

--- payload/subscaleFlight.py
+++ payload/subscaleFlight.py
@@ -19,19 +19,19 @@ with open("Firm_Log.csv", mode="w", newline="") as file:
     
     writer.writerow(["Time (s)", "Altitude (m)", "Accel X (m/s^2)", "Accel Y (m/s^2)", "Accel Z (m/s^2)"])
     
-while True:
-    # Get data packet from firm
-    data = firm.get_most_recent_data_packet()
-    pressure = data.pressure_pascals
-    temperature = data.temperature_celsius
-    
-    pressureRatio = P0 / pressure
-    eq1 = math.pow(pressureRatio,(1 / 5.257))
-    eq2 = temperature + 273.15
-    altitude = 44330*(1-eq1)
-    
-    # Log to CSV file WITH timestamps
-    
-    writer.writerow([data.timestamp_seconds, altitude, data.accel_x_meters_per_s2, data.accel_y_meters_per_s2, data.accel_z_meters_per_s2])
-    
-    time.sleep(0.02)
+    while True:
+        # Get data packet from firm
+        data = firm.get_most_recent_data_packet()
+        pressure = data.pressure_pascals
+        temperature = data.temperature_celsius
+        
+        pressureRatio = P0 / pressure
+        eq1 = math.pow(pressureRatio,(1 / 5.257))
+        eq2 = temperature + 273.15
+        altitude = 44330*(1-eq1)
+        
+        # Log to CSV file WITH timestamps
+        
+        writer.writerow([data.timestamp_seconds, altitude, data.accel_x_meters_per_s2, data.accel_y_meters_per_s2, data.accel_z_meters_per_s2])
+        
+        time.sleep(0.02)
