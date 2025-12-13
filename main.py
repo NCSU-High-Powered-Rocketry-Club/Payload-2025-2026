22160b7c8e3428726321fffc58a482b4966cf5c9
connors-01 <cnswans2@ncsu.edu>
2025-12-12 19:28:21 -0600

    Get it working with firm

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-from firm import FIRM
+from firm_client import FIRMClient
 import csv
 from imu_logger import state
 import math
 import time
+from datetime import datetime
 
 # Initialize FIRM
-firm = FIRM(port="/dev/ttyACM0")
-firm.initialize()
+firm = FIRMClient("/dev/ttyACM0")
+firm.get_data_packets()
+firm.zero_out_pressure_altitude()
+firm.start()
 
 # Open CSV file
-with open("Firm_Log.csv", mode="w", newline="") as file:
-    writer = csv.writer(file)
-    
-    writer.writerow(["Time (s)", "State", "Altitude (m)", "Accel X (m/s^2)", "Accel Y (m/s^2)", "Accel Z (m/s^2)"])
-    
+
 def main():
     max_altitude = 0
     current_state = state.StandbyState()
-    while True:
-        # Get data packet from firm
-        data = firm.get_most_recent_data_packet()
-        pressure = data.pressure_pascals
-        temperature = data.temperature_celsius
-        altitude = data.pressure_altitude_meters
-        max_altitude = max(max_altitude, data.pressure_altitude_meters)
+    current_date = datetime.now()
+    with open(f"Firm_Log_{current_date}.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Time (s)", "State", "Altitude (m)", "Accel X (m/s^2)", "Accel Y (m/s^2)", "Accel Z (m/s^2)"])
+        while True:
+            # Get data packet from firm
+            data = firm.get_data_packets(block=True)[-1]
+            pressure = data.pressure_pascals
+            temperature = data.temperature_celsius
+            altitude = data.pressure_altitude_meters
+            max_altitude = max(max_altitude, data.pressure_altitude_meters)
 
-        current_state.update(data, max_altitude)
-        
-        # Log to CSV file WITH timestamps
-        
-        writer.writerow([data.timestamp_seconds, current_state.name, altitude, data.accel_x_meters_per_s2, data.accel_y_meters_per_s2, data.accel_z_meters_per_s2])
+            current_state = current_state.update(data, max_altitude)
+            
+            # Log to CSV file WITH timestamps
+            
+            writer.writerow([data.timestamp_seconds, current_state.name, altitude, data.accel_x_meters_per_s2, data.accel_y_meters_per_s2, data.accel_z_meters_per_s2])
 
     
 if __name__ == "__main__":
     main()
+    firm.stop()
--- src/imu_logger/state.py
+++ src/imu_logger/state.py
@@ -3,13 +3,14 @@ Module for the finite state machine that represents which state of flight the ro
 """
 
 from abc import ABC, abstractmethod
+from typing import Self
 import time
 
 
 class State(ABC):
     """    """
 
-    __slots__ = ("start_time_s")
+    __slots__ = ()
 
     @property
     def name(self) -> str:
@@ -17,18 +18,10 @@ class State(ABC):
         return self.__class__.__name__
 
     @abstractmethod
-    def update(self, _, __) -> None:
+    def update(self, _, __) -> Self:
         """
         Called every loop iteration.
         """
-
-    @abstractmethod
-    def next_state(self) -> None:
-        """
-        We never expect/want to go back a state e.g. We're never going to go from Flight to Motor
-        Burn, so this method just goes to the next state.
-        """
-
 
 class StandbyState(State):
     """
@@ -37,16 +30,14 @@ class StandbyState(State):
 
     __slots__ = ()
 
-    def update(self, firm_packet, _) -> None:
+    def update(self, firm_packet, _) -> State:
         """
         Checks if the rocket has launched, based on our velocity.
         """
         acceleration = (firm_packet.accel_x_meters_per_s2**2 + firm_packet.accel_y_meters_per_s2**2 + firm_packet.accel_z_meters_per_s2**2)**0.5
         if acceleration > 50:
-            self.next_state()
-
-    def next_state(self):
-        self.context.state = MotorBurnState(self.context)
+            return MotorBurnState()
+        return self
 
 
 class MotorBurnState(State):
@@ -54,18 +45,16 @@ class MotorBurnState(State):
     When the motor is burning and the rocket is accelerating.
     """
 
-    __slots__ = ()
+    __slots__ = ("launch_time_s")
 
     def __init__(self):
         super().__init__()
         self.launch_time_s = time.time()
 
-    def update(self, _, __) -> None:
+    def update(self, _, __) -> State:
         if time.time() - self.launch_time_s > 1.6: # CHANGE THIS TO BE MORE PRECISE
-            self.next_state()
-
-    def next_state(self) -> None:
-        self.context.state = CoastState(self.context)
+            return CoastState()
+        return self 
 
 
 class CoastState(State):
@@ -77,10 +66,7 @@ class CoastState(State):
 
     __slots__ = ()
 
-    def __init__(self) -> None:
-        super().__init__()
-
-    def update(self, firm_packet, max_altitude) -> None:
+    def update(self, firm_packet, max_altitude) -> State:
         """
         Checks to see if the rocket has reached apogee, indicating the start of free fall.
         """
@@ -88,11 +74,8 @@ class CoastState(State):
         if (
             firm_packet.pressure_altitude_meters <= max_altitude * 0.90
         ):
-            self.next_state()
-            return
-
-    def next_state(self) -> None:
-        self.context.state = FreeFallState(self.context)
+            return FreeFallState()
+        return self
 
 
 class FreeFallState(State):
@@ -102,20 +85,16 @@ class FreeFallState(State):
 
     __slots__ = ()
 
-    def update(self, firm_packet, _) -> None:
+    def update(self, firm_packet, _) -> State:
         """
         Check if the rocket has landed, based on our altitude and a spike in acceleration.
         """
-        data = self.context.data_processor
         acceleration = (firm_packet.accel_x_meters_per_s2**2 + firm_packet.accel_y_meters_per_s2**2 + firm_packet.accel_z_meters_per_s2**2)**0.5
 
         # If our altitude is around 0, and we have an acceleration spike, we have landed
         if (firm_packet.pressure_altitude_meters <= 15 and acceleration >= 30):
-            self.next_state()
-
-
-    def next_state(self) -> None:
-        self.context.state = LandedState(self.context)
+            return LandedState()
+        return self
 
 
 class LandedState(State):
@@ -125,10 +104,8 @@ class LandedState(State):
 
     __slots__ = ()
 
-    def update(self, _, __) -> None:
+    def update(self, _, __) -> State:
         """
         We use this method to stop the air brakes system after we have hit our log buffer.
         """
-    def next_state(self) -> None:
-        # Explicitly do nothing, there is no next state
-        pass
+        return self
