68e9347cf80eb9800edf48a9e3ed13d3689e5729
benradspinner <benradspinner@gmail.com>
2026-03-24 21:03:38 -0400

    I am going to kill Will

--- payload/data_handling/packets/logger_data_packet.py
+++ payload/data_handling/packets/logger_data_packet.py
@@ -18,21 +18,11 @@ class LoggerDataPacket(msgspec.Struct, array_like=True, kw_only=True):
     state_letter: str | None
 
     # Estimated Data Packet Fields
-    est_acceleration_x_gs: float | None = None
-    est_acceleration_y_gs: float | None = None
-    est_acceleration_z_gs: float | None = None
-    est_angular_rate_x_rad_per_s: float | None = None
-    est_angular_rate_y_rad_per_s: float | None = None
-    est_angular_rate_z_rad_per_s: float | None = None
-    est_position_x_meters: float | None = None
-    est_position_y_meters: float | None = None
     est_position_z_meters: float | None = None
     est_quaternion_w: float | None = None
     est_quaternion_x: float | None = None
     est_quaternion_y: float | None = None
     est_quaternion_z: float | None = None
-    est_velocity_x_meters_per_s: float | None = None
-    est_velocity_y_meters_per_s: float | None = None
     est_velocity_z_meters_per_s: float | None = None
     magnetic_field_x_microteslas: float | None = None
     magnetic_field_y_microteslas: float | None = None
@@ -46,6 +36,8 @@ class LoggerDataPacket(msgspec.Struct, array_like=True, kw_only=True):
     raw_angular_rate_z_deg_per_s: float | None = None
     temperature_celsius: float | None = None
     timestamp_seconds: float | None = None
+    est_mach_number: float | None = None
+    est_tilt_angle_degrees: float | None = None
 
     # Grave packets
     position: int | None = None
